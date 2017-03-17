9e5f833659e13a2b87e6e2e9f05c04b645b0a331
Coman <jx8613@163.com>
2017-03-17 14:29:33 +0800

    chapter5 code of Pro Asp.net Mvc5

---
+++ cp5/RazorTest/RazorTest/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RazorTest.Models;
+
+namespace RazorTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            var newProduct = new Product()
+            {
+                Category = "Soccer",
+                Description = "Soccer",
+                Price = 200M,
+                ProductID = 100,
+                ProductName = "Kayam"
+            };
+            return View(newProduct);
+        }
+
+        public ActionResult RazorTest()
+        {
+            var proArrays = new Product[]
+            {
+                new Product()
+                {
+                    Category = "Soccer",
+                    Description = "Soccer",
+                    Price = 200M,
+                    ProductID = 100,
+                    ProductName = "Kayam"
+                },
+                new Product()
+                {
+                    Category = "Soccer",
+                    Description = "Soccer",
+                    Price = 100M,
+                    ProductID = 101,
+                    ProductName = "Addas"
+                }
+            };
+
+            return View(proArrays);
+        }
+    }
+}
---
+++ cp5/RazorTest/RazorTest/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RazorTest.Models
+{
+    public class Product
+    {
+        public int ProductID { set; get; }
+
+        public String ProductName { set; get; }
+
+        public String Description { set; get; }
+
+        public String Category { set; get; }
+
+        public decimal Price { set; get; }
+    }
+}
